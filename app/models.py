3f982d5a4c7cc8030bd304e7933413ee85e053fc
Travis Bumgarner <travis.bumgarner@gmail.com>
2017-03-04 00:34:46 -0500

    working through models

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     requests = db.relationship('User_Request', backref= 'user', lazy = 'dynamic')
 
