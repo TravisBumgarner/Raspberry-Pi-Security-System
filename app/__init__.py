d716e4582adc713e2c5fd4288afeadc1f6d68d37
Travis Bumgarner <travis.bumgarner@gmail.com>
2017-03-14 20:26:09 +0000

    Added cron job for generating thumbnails

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,6 @@ from flask_login import LoginManager
 from flask_admin import Admin
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-from threading import Thread
-from .functions import generate_thumbnails
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -28,9 +26,6 @@ limiter = Limiter(
     key_func=get_remote_address,
     global_limits=["200 per day", "50 per hour"]
 )
-
-# Thumbnails
-Thread(target = generate_thumbnails).start()
 
 from app import views, models, admin_panel
 
